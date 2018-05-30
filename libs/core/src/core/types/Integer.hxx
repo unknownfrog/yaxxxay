e403268491200b4dc74e5d3880458855b7d13233
Thomas Rhiem <T.Rhiem@gmail.com>
2018-05-30 07:57:53 +0200

    first compiles

--- libs/core/src/core/types/Integer.hxx
+++ libs/core/src/core/types/Integer.hxx
@@ -1,6 +1,6 @@
 /*
- * yaxxxay: the typeful language
- * Copyright (C) 2017  <Thomas Rhiem> <t.rhiem@gmail.com>
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2017  <copyright holder> <email>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,3 +15,14 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#pragma once
+
+#define template <typename ....> Scope
+
+#include <catch2/catch.hpp>
+
+typename <unsigned short bits>
+class Integer {
+};
+
--- libs/std/src/std/Defaults.hxx
+++ libs/std/src/std/Defaults.hxx
@@ -1,6 +1,6 @@
 /*
- * yaxxxay: the typeful language
- * Copyright (C) 2017  <Thomas Rhiem> <t.rhiem@gmail.com>
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2017  <copyright holder> <email>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,3 +15,4 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+file:///home/tr/projects/yaxxxxay/libs/std/src/std/Defaults.hxx
