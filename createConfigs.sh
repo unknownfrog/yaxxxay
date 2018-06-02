42d7e174e85c44f6fceb20b2b1e20a1b743f7699
Thomas Rhiem <T.Rhiem@gmail.com>
2018-06-02 15:46:44 +0200

    updated. workds again

--- createConfigs.sh
+++ createConfigs.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-compilers=`ls /usr/bin/gcc-[4-9]* /usr/bin/clang-[4-9].0`
+compilers=`ls /usr/bin/g++-[1-9]* /usr/bin/clang++-[6-9].[0-9]`
 
 buildModes="DEBUG RELEASE RELWITHDEBINFO MINSIZEREL"
 
-echo compiler = $compilers 
-echo buildmode = $buildModes
+# echo compiler = $compilers 
+# echo buildmode = $buildModes
 
 for compiler in $compilers;do
     name=`basename $compiler`
     for buildmode in $buildModes;do
        sourceDir=$(pwd)
-       dir=build/${name}-${buildmode}
+       config=${name}-${buildmode}
+       dir=build/${config}
        mkdir -p $dir
        pushd $dir
+       echo configure $config
        cmake -GNinja -DCMAKE_CXX_COMPILER=$compiler -DCMAKE_BUILD_TYPE=$buildmode . $sourceDir
        popd
     done
