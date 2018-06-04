0a320d65ce3a23a7e1d9f176041c500ab1a4b6c6
Thomas Rhiem <t.rhiem@gmail.com>
2018-06-04 07:42:48 +0200

    added catch to git as submodule

--- libs/src/core/Scope.hxx
+++ libs/src/core/Scope.hxx
@@ -19,6 +19,7 @@
 #pragma once
 
 #include <catch.hpp>
+#include <core/Scope.hxx>
 
 
 namespace NAMESPACE {
--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <core/Scope.hxx>
 
 int main(int, char**) {
+    core::ScopeCardinal scope = ScopeCardinal::literal_;
+    
     std::cout << "Hello, world!" << std::endl;
 
     return 0;
