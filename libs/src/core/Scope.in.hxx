e78d6014df9f7b4471e91377be0114dc57516cb6
Thomas Rhiem <t.rhiem@gmail.com>
2018-08-09 07:42:11 +0200

    path defines namespace of a source file, together with globbing. requires cmake 3.12

--- libs/src/core/Scope.in.hxx
+++ libs/src/core/Scope.in.hxx
@@ -1,73 +1,77 @@
+#line 1 "@ORIGFILENAME@"
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2017  <copyright holder> <email>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* yaxxxay, a solid, typeful, multi purpose, multi paradigm language
+* Copyright (C) 2017  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
-#line 21 "@ORIGFILENAME@"
-#include <catch2/catch.hpp>
+
+#if defined UNITTEST
+#  define CATCH_CONFIG_MAIN
+#  include <catch.hpp>
+#else
+#  define CATCH_CONFIG_DISABLE
+#endif
 
 namespace @NAMESPACE@ {
-    
-    enum class LocalScope {
-        on_stack,
-        on_heap
-    };
-        
-    enum class Scopes {
-        literal,      // a literal value, has not even an adress
-        const_,       // a immutable value, value is set at construction and immutable from then
-        local,        // a local value. only valid in local scope
-        owned,        // a heap allocated value. Only owner has access.
-        shared,       // a value which might be shared inside a single thread/task
-        communicate,  // a value used to communicate between threads/tasks/processes. Needs synchronisatin. TODO in progress
-    };
+
+enum class LocalScope {
+    on_stack,
+    on_heap
+};
+
+enum class Scopes {
+    literal,      // a literal value, has not even an adress
+    const_,       // a immutable value, value is set at construction and immutable from then
+    local,        // a local value. only valid in local scope
+    owned,        // a heap allocated value. Only owner has access.
+    shared,       // a value which might be shared inside a single thread/task
+    communicate,  // a value used to communicate between threads/tasks/processes. Needs synchronisatin. TODO in progress
+};
 
 
-    enum class Accessbility {
-        constant,
-        view,
-        change
-    };
+enum class Accessbility {
+    constant,
+    view,
+    change
+};
 
-    enum class ValueOrReference {
-        value,
-        refercence
-    };
+enum class ValueOrReference {
+    value,
+    refercence
+};
 
-    enum class StorageProvider {
-        stack,
-        heap
-    };
-    
-    // TODO
-    using Byte = unsigned char;
+enum class StorageProvider {
+    stack,
+    heap
+};
 
-    template<Scopes _scope>
-    class Scope {
-        static constexpr Scopes scope = _scope;
+// TODO
+using Byte = unsigned char;
 
-    };
+template<Scopes _scope>
+class Scope {
+    static constexpr Scopes scope = _scope;
 
-    using Literal = Scope<Scopes::literal>; 
-    using Const = Scope<Scopes::const_>;
-    using Mutale = Scope<Scopes::local>;
-    using Owned = Scope<Scopes::owned>;
-    using Shared = Scope<Scopes::shared>; 
-    using Communicator = Scope<Scopes::communicate>; 
+};
 
-    
+using Literal = Scope<Scopes::literal>;
+using Const = Scope<Scopes::const_>;
+using Mutale = Scope<Scopes::local>;
+using Owned = Scope<Scopes::owned>;
+using Shared = Scope<Scopes::shared>;
+using Communicator = Scope<Scopes::communicate>;
 }
--- libs/src/std/Defaults.in.hxx
+++ libs/src/std/Defaults.in.hxx
@@ -1,18 +1,31 @@
+#line 1 "@ORIGFILENAME@"
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2017  <copyright holder> <email>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-file:///home/tr/projects/yaxxxxay/libs/std/src/std/Defaults.hxx
+* yaxxxay, a solid, typeful, multi purpose, multi paradigm language
+* Copyright (C) 2017  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#if defined UNITTEST
+#  define CATCH_CONFIG_MAIN
+#  include <catch.hpp>
+#else
+#  define CATCH_CONFIG_DISABLE
+#endif
+
+namespace @NAMESPACE@ {
+
+}
